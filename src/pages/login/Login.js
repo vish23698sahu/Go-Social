a14c9519afdde63c259d343549507f38565483da
Vishakha Sahu <vishakhasahu023@gmail.com>
2023-03-13 23:24:48 +0530

    v - 5 Login component

---
+++ src/pages/login/Login.js
@@ -0,0 +1,43 @@
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import SignInButton from '../../components/signInButton/SignInButton';
+import { UserContext } from '../../contexts/user';
+import './Login.css';
+
+export default function Login() {
+    const { login } = useContext(UserContext);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState();
+
+    const loginClickHandler = async (e) => {
+        e.preventDefault();
+        try {
+            const result = await login(email, password)
+            console.log(result);
+        }
+        catch (error) {
+            console.log('Failed to sign in ', error);
+        }
+    }
+
+    return (
+        <div className='login__container' >
+            <div className='login__box' >
+                <form>
+                    <h2 className='login__h2' >Login</h2>
+                    <input className='login__email' type='text' value={email} placeholder='email' onChange={(e) => setEmail(e.target.value)} /><br /><br />
+                    <input className='login__password' type='password' password={password} placeholder='password' onChange={(e) => setPassword(e.target.value)} /><br />
+
+                    <button className='login__login' onClick={loginClickHandler} >Login</button><br /><br /><br />
+                    <hr /><br />
+                    <div className='login__google' ><SignInButton /></div>
+                    <hr /><br /><br />
+                    <div>
+                        Don't have an account ? <Link to='/register' className='login__signUp'>Sign Up</Link>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    )
+}
