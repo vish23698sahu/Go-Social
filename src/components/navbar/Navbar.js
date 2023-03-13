77e13ead98596157908ca2a8702a9cd399c44590
Vishakha Sahu <vishakhasahu023@gmail.com>
2023-03-13 23:23:06 +0530

    v - 3 navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom';
+import { UserContext } from '../../contexts/user';
+import './Navbar.css';
+
+export default function Navbar() {
+    const [user] = useContext(UserContext).user;
+
+    return (
+        <div className='navbar'>
+            <Link to='/' className='navbar__logo' >Go Social</Link>
+
+            {user ? <p className='navbar__home' > <span className='navbar__homeColor' >Welcome Home</span> {user.displayName} !</p> : ''}
+            {user ? <img className='navbar__img' src={user.photoURL} alt='user' />
+                :
+                <Link className='navbar__login' to='/login' >Log In</Link>
+            }
+        </div>
+    )
+}
+
