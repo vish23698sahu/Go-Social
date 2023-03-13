63f35f33f9d697697c926aa20d65cfe43f2f25b3
Vishakha Sahu <vishakhasahu023@gmail.com>
2023-03-13 23:27:08 +0530

    v - 9 Footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <div>
+            <p className='footer'>@Go Social 2023.</p>
+        </div>
+    )
+}
