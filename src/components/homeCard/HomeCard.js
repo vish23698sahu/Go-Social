a8d9707f364149786c8cc64a6ff12ac0a88851b8
Vishakha Sahu <vishakhasahu023@gmail.com>
2023-05-24 19:35:16 +0530

    v - 23 Home content updated

--- src/components/homeCard/HomeCard.js
+++ src/components/homeCard/HomeCard.js
@@ -1,9 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react';
+import SignInButton from '../signInButton/SignInButton';
 import { UserContext } from '../../contexts/user';
 import { db } from '../../firebase';
 import Post from '../post/Post';
-import SignInButton from '../signInButton/SignInButton';
 import './HomeCard.css';
+import { BsFillArrowRightCircleFill } from 'react-icons/bs';
 
 export default function HomeCard(props) {
     const [posts, setPosts] = useState([]);
@@ -20,8 +21,15 @@ export default function HomeCard(props) {
     }, []);
 
     return (
-        <div >
-            {!user && <div> <p className='home__p'>Lets share our current working items, progress, and ideas.</p><p className='home__p2'>P.S. No one can ever steal it from you.</p> </div>}
+        <div className='homeCard_main'>
+            {!user &&
+                <div>
+                    <p className='home__p'>GoSocial is a social platform for sharing your creativity and productivity journey.<br />
+                        Engage, interact, and inspire others as you express your ideas, achievements, and challenges.<br />
+                        It's where work meets passion, innovation meets collaboration, and you meet the world.
+                    </p>
+                </div>
+            }<br />
 
             <div className='homeCard__signIn'>
                 <SignInButton />
@@ -42,6 +50,96 @@ export default function HomeCard(props) {
                     />
                 })}
             </div>
+
+            <div className='home__Cardcontainer'>
+                <div className='homeCard__Card'>
+                    <div className='card_text'>
+                        <div className='text_container'>
+                            <h3>Share Your Progress</h3>
+                            <p>
+                                GoSocial is a space to showcase your work.
+                                Are you in the middle of an exciting project?
+                                Share your work-in-progress and receive constructive feedback from our supportive community.
+                                Keep everyone updated with your progress and watch as your ideas come to life!
+                            </p>
+                        </div>
+                        <div>
+                            <BsFillArrowRightCircleFill className='iconn' />
+                        </div>
+                    </div>
+                </div>
+                <div className='homeCard__Card'>
+                    <div className='card_text'>
+                        <div>
+                            <div className='text_container'>
+                                <h3>Spread Your Ideas</h3>
+                                <p>
+                                    Got a creative idea or an innovative solution?
+                                    Share it with the world on GoSocial!
+                                    Our platform connects you with like-minded individuals who can help bring your ideas to fruition.
+                                    Let's start a brainstorming revolution together!
+                                </p>
+                            </div>
+                        </div>
+                        <div>
+                            <BsFillArrowRightCircleFill className='iconn' />
+                        </div>
+                    </div>
+                </div>
+                <div className='homeCard__Card'>
+                    <div className='card_text'>
+                        <div className='text_container'>
+                            <div>
+                                <h3>Learn and Grow</h3>
+                                <p>
+                                    Your journey doesn't have to be alone.
+                                    GoSocial offers a rich community of experts and enthusiasts alike,
+                                    all willing to share their experience and knowledge.
+                                    Engage in insightful discussions, ask questions, provide answers, and learn from the best.
+                                </p>
+                            </div>
+                        </div>
+                        <div>
+                            <BsFillArrowRightCircleFill className='iconn' />
+                        </div>
+                    </div>
+                </div>
+                <div className='homeCard__Card'>
+                    <div className='card_text'>
+                        <div className='text_container'>
+
+                            <div>
+                                <h3>Collaborate and Network</h3>
+                                <p>
+                                    Beyond sharing and learning, GoSocial opens doors to collaboration and networking.
+                                    Connect with professionals and enthusiasts from different industries and areas of expertise.
+                                    Work together on shared interests, or simply form connections that could lead to incredible opportunities!
+                                </p>
+                            </div>
+                        </div>
+                        <div>
+                            <BsFillArrowRightCircleFill className='iconn' />
+                        </div>
+                    </div>
+                </div>
+                <div className='homeCard__Card'>
+                    <div className='card_text'>
+                        <div className='text_container'>
+                            <div>
+                                <h3>Celebrate Your Success</h3>
+                                <p>
+                                    Every milestone matters! Celebrate your accomplishments and share your success stories with the GoSocial community.
+                                    Let your victories, big or small, inspire others and pave the way for collective growth.
+                                </p>
+                            </div>
+                        </div>
+                        <div>
+                            <BsFillArrowRightCircleFill className='iconn' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 }
